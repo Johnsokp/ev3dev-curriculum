9aedef6188f01d575a78b285f4ece661f80495b8
coversc <coversc@rose-hulman.edu>
2018-02-02 18:03:55 -0500

    m4 partial

--- sandbox/m4/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/m4/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -3,8 +3,8 @@
 In this module you will use the touch sensor to make arm movements.  Instead of writing code from scratch you will
 fix the existing code, which is FULL of bugs.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Group 24.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -73,14 +73,18 @@ def arm_calibration(arm_motor, touch_sensor):
     #   Set the arm encoder position to 0 (the last line below is correct to do that, it's new so no bug there)
 
     # Code that attempts to do this task but has MANY bugs (nearly 1 on every line).  Fix them!
-    arm_motor.run_forever(speed_sp=100)
-    while not touch_sensor:
+
+    arm_motor.run_forever(MAX_SPEED)
+
+    while not touch_sensor.is_pressed:
         time.sleep(0.01)
-    arm_motor.stop(stop_action="coast")
+    arm_motor.stop(stop_action="brake")
+    ev3.Sound.beep()
 
     arm_revolutions_for_full_range = 14.2
     arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)
     arm_motor.wait_while(ev3.Motor.STATE_STALLED)
+    ev3.Sound.beep()
 
     arm_motor.position = 0  # Calibrate the down position as 0 (this line is correct as is).
 
@@ -102,9 +106,10 @@ def arm_up(arm_motor, touch_sensor):
 
     # Code that attempts to do this task but has many bugs.  Fix them!
     arm_motor.run_to_rel_pos(position_sp=14.2, speed_sp=MAX_SPEED)
-    while touch_sensor.is_pressed:
+    while not touch_sensor.is_pressed:
         time.sleep(0.01)
-    arm_motor.stop()
+    arm_motor.stop(stop_action="brake")
+    ev3.Sound.beep()
 
 
 def arm_down(arm_motor):
