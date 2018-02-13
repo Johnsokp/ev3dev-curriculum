aae2e8ce9b5843c013715c56ead2bdd34e1dd169
coversc <coversc@rose-hulman.edu>
2018-02-13 18:10:16 -0500

    caitlin pc code

--- projects/coversc/ev3 project.py
+++ projects/coversc/ev3 project.py
@@ -1,113 +1,52 @@
 import robot_controller as robo
+import mqtt_remote_method_calls as com
 
-
-COLOR_NAMES = ["None", "Black", "Blue", "Green", "Yellow", "Red", "White", "Brown"]
+import ev3dev.ev3 as ev3
+import time
 
 class DataContainer(object):
-    """ Helper class that might be useful to communicate between different callbacks."""
 
     def __init__(self):
         self.running = True
 
+    def drive_to_color(self, color_to_seek):
+        print("Received: {} {}".format(color_to_seek))
+
+        while robot.color_sensor.color != color_to_seek:
+            robot.forward(100, 100)
+            print(robot.color_sensor.color)
+        robot.stop()
+
 def main():
     robot = robo.Snatch3r()
     dc = DataContainer()
+
+    ev3_delegate = DataContainer()
+    mqtt_client = com.MqttClient(ev3_delegate)
+    mqtt_client.connect_to_pc()
 
     print("--------------------------------------------")
     print("Let's run errands")
     print("--------------------------------------------")
     ev3.Sound.speak("Let's run errands").wait()
 
-    white_level = 50
-    black_level = 40
-
     while dc.running:
         btn.process()
         time.sleep(0.01)
 
-    #while True:
-    #    command_to_run = input("Enter w (white), b (black), store, school,
-        # home or quit: ")
-    #    if command_to_run == 'w':
-    #        print("Calibrate the white light level")
+    btn = ev3.Button()
+    btn.on_up = lambda state: handle_button_press(state, mqtt_client, "Up")
+    btn.on_down = lambda state: handle_button_press(state, mqtt_client, "Down")
+    btn.on_left = lambda state: handle_button_press(state, mqtt_client, "Left")
+    btn.on_right = lambda state: handle_button_press(state, mqtt_client,
+                                                     "Right")
+    btn.on_backspace = lambda state: handle_shutdown(state, ev3_delegate)
 
-    #        print(robot.color_sensor.reflected_light_intensity)
-
-    #        white_level = robot.color_sensor.reflected_light_intensity
-
-    #        print("New white level is {}.".format(white_level))
-    #    elif command_to_run == 'b':
-    #        print("Calibrate the black light level")
-
-     #       print(robot.color_sensor.reflected_light_intensity)
-
-    #        black_level = robot.color_sensor.reflected_light_intensity
-
-     #       print("New black level is {}.".format(black_level))
-
-     #   elif command_to_run == 'store':
-     #       print("Going to the store")
-     #       ev3.Sound.speak("Going to the store").wait()
-     #       follow_the_line(robot, white_level, black_level)
-     #   elif command_to_run == 'school':
-     #       print("Going to school")
-      #      ev3.Sound.speak("Going to school").wait()
-    #    elif command_to_run == 'home':
-     #       print("Going home")
-       #     ev3.Sound.speak("Going to school").wait()
-     #       follow_the_line(robot, white_level, black_level)
-     #   elif command_to_run == 'quit':
-     #       break
-    #    else:
-    #        print(command_to_run, "is not a known command. Please enter a
-    # valid choice.")
-
-    print("Goodbye!")
-    ev3.Sound.speak("Goodbye").wait()
-
-def drive_to_color(button_state, robot, color_to_seek):
-    """
-    When the button_state is True (pressed), drives the robot forward until the desired color is detected.
-    When the color_to_seek is detected the robot stops moving forward and speaks a message.
-
-    Type hints:
-      :type button_state: bool
-      :type robot: robo.Snatch3r
-      :type color_to_seek: int
-    """
+def handle_button_press(button_state, mqtt_client, button_name):
     if button_state:
-        ev3.Sound.speak("Seeking " + COLOR_NAMES[color_to_seek]).wait()
-
-        while robot.color_sensor.color != color_to_seek:
-            robot.forward(100, 100)
-            print(robot.color_sensor.color)
-
-        robot.stop()
-    ev3.Sound.speak("Found " + COLOR_NAMES[color_to_seek]).wait()
-
-def follow_the_line(robot, white_level, black_level):
-    """
-    The robot follows the black line until the touch sensor is pressed.
-    You will need a black line track to test your code
-    When the touch sensor is pressed, line following ends, the robot stops, and control is returned to main.
-
-    Type hints:
-      :type robot: robo.Snatch3r
-      :type white_level: int
-      :type black_level: int
-    """
-
-    x = 1
-    while x == 1:
-        if robot.color_sensor.reflected_light_intensity >= black_level + 20:
-            robot.turn_degrees(-10, 200)
-        else:
-            robot.forward(600, 600)
-        if robot.touch_sensor.is_pressed:
-            break
-
-    robot.stop()
-    ev3.Sound.speak("Done")
+        print("{} button was pressed".format(button_name))
+        mqtt_client.send_message("button_pressed",
+                                 [button_name])
 
 def quit_program(mqtt_client, shutdown_ev3):
     if shutdown_ev3:
@@ -117,9 +56,4 @@ def quit_program(mqtt_client, shutdown_ev3):
     exit()
 
 
-
-
-# ----------------------------------------------------------------------
-# Calls  main  to start the ball rolling.
-# ----------------------------------------------------------------------
 main()
--- projects/coversc/pc_project.py
+++ projects/coversc/pc_project.py
@@ -10,18 +10,8 @@ class DataContainer(object):
 
     def __init__(self, label_to_display_messages_in):
         self.running = True
-        self.display_label = label_to_display_messages_in
-
-    def button_pressed(self, button_name):
-        print("Received: " + button_name)
-        message_to_display = "{} was pressed.".format(button_name)
-        self.display_label.configure(text=message_to_display)
 
 def main():
-
-    pc_delegate = DataContainer(button_message)
-    mqtt_client = com.MqttClient(pc_delegate)
-    mqtt_client.connect_to_ev3()
 
     root = tkinter.Tk()
     root.title("Let's Run Errands")
@@ -29,50 +19,37 @@ def main():
     main_frame = ttk.Frame(root, padding=20, relief='raised')
     main_frame.grid()
 
+    button_message = ttk.Label(main_frame, text="")
+    button_message.grid(row=2, column=1)
+
     go_to_the_store_button = ttk.Button(main_frame, text="Go to the Store")
     go_to_the_store_button.grid(row=0, column=0)
-    go_to_the_store_button['command'] = lambda: color_seek(
+    go_to_the_store_button['command'] = lambda: send_color_demand(
         mqtt_client, 'blue')
 
     go_to_school_button = ttk.Button(main_frame, text="Go to school")
     go_to_school_button.grid(row=0, column=2)
-    go_to_school_button['command'] = lambda: color_seek(
+    go_to_school_button['command'] = lambda: send_color_demand(
         mqtt_client, 'green')
 
     go_home_button = ttk.Button(main_frame, text="Go home")
     go_home_button.grid(row=2, column=0)
-    go_home_button['command'] = lambda: color_seek(
+    go_home_button['command'] = lambda: send_color_demand(
     mqtt_client, 'red')
 
     q_button = ttk.Button(main_frame, text="Quit")
     q_button.grid(row=2, column=2)
     q_button['command'] = lambda: quit_program(mqtt_client, False)
 
+    pc_delegate = DataContainer(button_message)
+    mqtt_client = com.MqttClient(pc_delegate)
+    mqtt_client.connect_to_ev3()
+
     root.mainloop()
 
 def send_color_demand(mqtt_client, color_to_seek):
     print('Seeking LED color = {}'.format(color_to_seek))
     mqtt_client.send_message("drive_to_color", [color_to_seek])
-
-def drive_to_color(mqtt_client, color_to_seek):
-    """
-    When the button_state is True (pressed), drives the robot forward until the desired color is detected.
-    When the color_to_seek is detected the robot stops moving forward and speaks a message.
-
-    Type hints:
-      :type button_state: bool
-      :type robot: robo.Snatch3r
-      :type color_to_seek: int
-    """
-    if button_state:
-        ev3.Sound.speak("Seeking " + COLOR_NAMES[color_to_seek]).wait()
-
-        while robot.color_sensor.color != color_to_seek:
-            robot.forward(100, 100)
-            print(robot.color_sensor.color)
-
-        robot.stop()
-    ev3.Sound.speak("Found " + COLOR_NAMES[color_to_seek]).wait()
 
 def quit_program(mqtt_client, shutdown_ev3):
     if shutdown_ev3:
