c9368f20421761336143be5f06860aac67ec6c66
coversc <coversc@rose-hulman.edu>
2018-02-13 19:51:30 -0500

    caitlin pc code

--- projects/coversc/ev3 project.py
+++ projects/coversc/ev3 project.py
@@ -6,23 +6,31 @@ import time
 
 class DataContainer(object):
 
-    def __init__(self):
+    def __init__(self, r):
         self.running = True
+        self.rrr = r
 
     def drive_to_color(self, color_to_seek):
         print("Received: {}".format(color_to_seek))
 
-        while robo.color_sensor.color != color_to_seek:
-            self.forward(100, 100)
-            print(self.color_sensor.color)
-        self.stop()
+        if color_to_seek == 'blue':
+            color_number = 2
+        elif color_to_seek == 'green':
+            color_number = 3
+        elif color_to_seek == 'red':
+            color_number = 5
+
+        while self.rrr.color_sensor.color != color_number:
+            self.rrr.forward(100, 100)
+            print(self.rrr.color_sensor.color)
+        print('stop')
+        self.rrr.stop()
 
 def main():
     robot = robo.Snatch3r()
     dc = DataContainer(robot)
 
-    ev3_delegate = DataContainer()
-    mqtt_client = com.MqttClient(ev3_delegate)
+    mqtt_client = com.MqttClient(dc)
     mqtt_client.connect_to_pc()
 
     print("--------------------------------------------")
