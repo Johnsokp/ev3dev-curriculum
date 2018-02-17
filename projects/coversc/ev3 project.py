f453badfb5f386dadc09eb146391af83c5447e02
coversc <coversc@rose-hulman.edu>
2018-02-16 19:32:20 -0500

    caitlin pc code

--- projects/coversc/ev3 project.py
+++ projects/coversc/ev3 project.py
@@ -22,45 +22,46 @@ class DataContainer(object):
         elif color_to_seek == 'red':
             color_number = 5
 
-        while True:
-            if self.rrr.touch_sensor.is_pressed:
-                self.rrr.stop()
-                return
-            if color_to_seek == color_number:
-                break
-
         while self.rrr.color_sensor.color != color_number:
-            self.rrr.forward(100, 100)
+            self.rrr.forward(200, 200)
             print(self.rrr.color_sensor.color)
         print('stop')
 
         if color_number == 2:
+            self.rrr.forward(30, self.speed)
             self.rrr.turn_degrees(-90, self.speed)
-            self.rrr.drive_inches(24, self.speed)
+            self.rrr.drive_inches(18, self.speed)
             self.rrr.arm_up()
             self.rrr.arm_down()
             # added code
+            self.rrr.turn_degrees(180, self.speed)
+            self.rrr.drive_inches(18, self.speed)
             self.rrr.turn_degrees(90, self.speed)
-            self.rrr.drive_inches(24, self.speed)
             self.rrr.seek_beacon()
+            self.rrr.drive_inches(-5, self.speed)
+            self.rrr.turn_degrees(180, self.speed)
         elif color_number == 3:
             self.rrr.turn_degrees(90, self.speed)
-            self.rrr.drive_inches(36, self.speed)
+            self.rrr.drive_inches(24, self.speed)
             ev3.Sound.speak("We're learning today").wait()
             #added code
-            self.rrr.turn_degrees(90, self.speed)
-            self.rrr.drive_inches(36, self.speed)
+            self.rrr.turn_degrees(180, self.speed)
+            self.rrr.drive_inches(24, self.speed)
+            self.rrr.turn_degrees(-90, self.speed)
             self.rrr.seek_beacon()
+            self.rrr.drive_inches(-5, self.speed)
+            self.rrr.turn_degrees(180, self.speed)
         elif color_number == 5:
             self.rrr.turn_degrees(90, self.speed)
             self.rrr.drive_inches(20,  self.speed)
-            self.rrr.turn_degrees(180, self.speed)
+            self.rrr.turn_degrees(430, self.speed)
+            ev3.Sound.speak('Home for the night!').wait()
 
-        def go_home(self):
-            self.rrr.seek_beacon()
+    def go_home(self):
+        self.rrr.seek_beacon()
 
-        def set_speed(self, speed):
-            self.speed = speed
+    def set_speed(self, speed):
+        self.speed = speed
 
 def main():
     robot = robo.Snatch3r()
